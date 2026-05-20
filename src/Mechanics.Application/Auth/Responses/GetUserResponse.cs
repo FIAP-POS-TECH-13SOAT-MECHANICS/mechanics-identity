a293d7508625e536a484592487b7064b17ad0535
Augusto César Bisognin <augustocb@gmail.com>
2026-05-19 22:53:25 -0300

    retornar e-mail do usuário na consulta por ID

--- src/Mechanics.Application/Auth/Responses/GetUserResponse.cs
+++ src/Mechanics.Application/Auth/Responses/GetUserResponse.cs
@@ -5,5 +5,6 @@ public class GetUserResponse
     public required Guid Id { get; init; }
     public required string FullName { get; init; }
     public required string CpfNumber { get; init; }
+    public required string Email { get; init; }
     public required RoleResponse Role { get; init; }
 }
--- tests/Mechanics.Tests.Unit/Tests/Auth/UserAppServiceTests.cs
+++ tests/Mechanics.Tests.Unit/Tests/Auth/UserAppServiceTests.cs
@@ -167,6 +167,7 @@ public class UserAppServiceTests
         Assert.AreEqual(userId, response.Id);
         Assert.AreEqual(user.FullName, response.FullName);
         Assert.AreEqual(user.CpfNumber, response.CpfNumber);
+        Assert.AreEqual(user.Email, response.Email);
         Assert.IsNotNull(response.Role);
         Assert.AreEqual(RoleNames.Mechanic, response.Role.Name);
     }
